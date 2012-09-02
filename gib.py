a3653832f7a7cb9fad7600d225f7ff172ea73691
Mark Tully <mark@tntbasic.com>
2012-09-02 01:26:15 +0100

    added license and some tidy up

--- gib.py
+++ gib.py
@@ -1,3 +1,32 @@
+#!/usr/bin/python
+
+# gib.py
+# A backup script that uses git
+# Mark Tully
+# 2/9/12
+
+#===============================================================================
+# Copyright (C) 2012 by Mark Tully
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#===============================================================================
+
 from pbs import git
 import sys,os,datetime
 
@@ -31,12 +60,12 @@ def makeTreeFromDir(backupname,paths):
             fileHash=git('hash-object','-w',path).strip()
             filesInTree.append((bn,fileHash))
     clearIndex()
-    # now make the index
+    # now make the final snapshot index
     for (dir,tree) in dirsInTree:
         git('read-tree',tree,'--prefix=%s/'%dir)
     for (file,hash) in filesInTree:
         # see http://git-scm.com/book/en/Git-Internals-Git-Objects
-        # adding with 10644 means normal file
+        # adding with 10644 means normal file (TODO perhaps check if it should be marked as executable)
         # cacheinfo means we have the hash, but no file in our work dir corresponding to it
         git('update-index','--add','--cacheinfo','10644',hash,file)
     tree=git('write-tree').strip()
@@ -117,22 +146,29 @@ def extract():
         sys.exit(1)
 
 def usage():
-    print 'usage:'
+    print 'gib 0.1'
+    print '   A backup tool that uses git. Run from inside a git repro to backup'
+    print '   files into the repro'
+    print
+    print 'Usage:'
+    print
     print 'gib snapshot <backupname> <path to backup>+'
-    print ' will take a snapshot of the given path(s) and save it'
-    print ' directories will be recursively backed up and placed in a dir at the'
-    print ' root level of the snapshot'
-    print ' if a path is a file, the file will be backed up to the root level of'
-    print ' the snapshot'
-    print ' it will write the tree to refs/gib/backupname/snapshots/YYYYMMDD_HHMMSS'
+    print '  will take a snapshot of the given path(s) and save it'
+    print '  directories will be recursively backed up and placed in a dir at the'
+    print '  root level of the snapshot'
+    print '  if a path is a file, the file will be backed up to the root level of'
+    print '  the snapshot'
+    print '  it will write the tree to refs/gib/backupname/snapshots/YYYYMMDD_HHMMSS'
+    print
     print 'gib list [backupname]'
-    print ' will list all available snapshots for [backupname]'
-    print ' if backupname is ommitted, it will list all available snapshots for all backups'
-    print 'gib list [backupname]'
-    print ' will list all available snapshots for [backupname]'
-    print ' if backupname is ommitted, it will list all available snapshots for all backups'
+    print '  will list all available snapshots for [backupname]'
+    print '  if backupname is ommitted, it will list all available snapshots for all'
+    print '  backups'
+    print
     print 'gib extract <backupname> <snapshotname> <destdir>'
-    print ' extract a backup to the directory named. <destdir> must not already exist'
+    print '  extract a backup to the directory <destdir>'
+    print '  <destdir> must not already exist'
+    print
 
 if __name__ == "__main__":
     if not os.path.isdir('.git'):
@@ -147,7 +183,7 @@ if __name__ == "__main__":
         list_()
     elif sys.argv[1]=='extract':
         extract()
-    elif sys.argv[1]=='help':
+    elif sys.argv[1]=='help' or sys.argv[1]=='--help':
         usage()
     else:
         print 'Unknown command %s'%sys.argv[1]
